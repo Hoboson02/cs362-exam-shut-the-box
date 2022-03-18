893b735aa91dfea76f7efa2579329dd715c40034
Ryan Earl <ryanleoearl@gmail.com>
2022-03-17 19:36:47 -0700

    Previous attempted method for first solution was causing problems with the second, so I scrapped it and simplified. Now tile_set returns an array of 9 tiles by default and returns an array of tiles of a specified length

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -1,15 +1,15 @@
 require_relative '../lib/tile_set'
 
 describe 'A tile set' do
-  let(:tile_set) { TileSet.new() }
+  
   describe '::generate' do
-
     it 'returns an array of 9 tiles by default' do
-      tile_set
+      tile_set = TileSet.new
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      tile_set = TileSet.generate(21)
+      expect(tile_set.length).to eq(21)
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
